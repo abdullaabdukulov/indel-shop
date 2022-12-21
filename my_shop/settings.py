727f2d62ea2e2e8c5c0fba2f66a36857517a10af
Abdulla Abdukulov <abduqulovabdulla3108@gmail.com>
2022-12-21 11:49:15 +0500

    changed by Abdullah

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -135,9 +135,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
+STATIC_URL = 'static/'
+if DEBUG:
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+       ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/'
 
